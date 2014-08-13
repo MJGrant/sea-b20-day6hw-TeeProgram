d4026eb7249deda97d722a258cbdbd1b0561bf26
Mandi Grant <mandi018@gmail.com>
2014-08-13 11:52:38 -0700

    Completed assignment

---
+++ teeprogram.js
@@ -0,0 +1,17 @@
+//To use: 
+//$ node teeprogram.js "your string here"
+//"your string here" will be returned to the console and output to outputfile.txt
+'use strict';
+
+var fs = require('fs');
+var input = process.argv[2];
+
+
+
+var stream = fs.createWriteStream("output.txt");
+stream.once('open', function(write) {
+  stream.write(input);
+  stream.end();
+});
+
+console.log("Your input \"" + input + "\"\ has been written to output.txt.");
